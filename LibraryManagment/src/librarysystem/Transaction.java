eab644b3b3b3523cb66025f75100acaac83c9935
ZournaGs <zournatzisiwn15@gmail.com>
2025-04-20 17:49:05 +0300

    Uploading the project

---
+++ LibraryManagment/src/librarysystem/Transaction.java
@@ -0,0 +1,72 @@
+package librarysystem;
+
+public class Transaction {
+    private User user_t;
+    private Book book_t;
+    private TransactionType type; // "BORROW or "BUY"
+    //Date is handled by DB
+    private Boolean completed;
+    
+  //Constructors, no nulls are allowed
+    public Transaction(User user_t, Book book_t, TransactionType type, Boolean completed) {
+    	if (user_t == null || book_t == null || type == null || completed == null) {
+            throw new IllegalArgumentException("No null values allowed in Transaction.");
+        }
+    	this.user_t=user_t;
+    	this.book_t=book_t;
+    	this.type=type;
+    	this.completed=completed;
+    }
+    
+    //Getters
+    public User getUser_t(){
+    	return this.user_t;
+    }
+    
+    public Book getBook_t() {
+    	return this.book_t;
+    }
+    
+    public TransactionType getType() {
+    	return this.type;
+    }
+    
+    public Boolean getCompleted() {
+    	return completed;
+    }
+    
+    //Setters
+    public void setUser_t(User user) {
+    	this.user_t=user;
+    }
+    
+    public void setBook_t(Book book) {
+    	this.book_t=book;
+    }
+    
+    public void setType(TransactionType type) {
+    	this.type=type;
+    }
+    
+    public void setCompleted(Boolean completed) {
+    	this.completed=completed;
+    }
+    
+    //Show Transaction
+    
+    public void ShowValues() {
+    	String[] data= {
+        		this.user_t.getUsrname(),
+        		this.book_t.getTitle(),
+        		String.valueOf(type),
+        		String.valueOf(completed)
+        	};
+        	//print in series in a for loop
+        	for(int i=0;i<data.length;i++) {
+        		System.out.println(data[i]);
+        }
+    }
+    
+}
+
+
