b0dc98a980c8c3c55d6d3ec3cbe208f4b4838fe6
ZournaGs <zournatzisiwn15@gmail.com>
2025-05-17 01:57:35 +0300

    USERS TABLE FIXED AND ADDED THE REST+ TRANSACTION TYPE ENUM

--- src/database/TABLES/USERS.sql
+++ src/database/TABLES/USERS.sql
@@ -1,7 +1,8 @@
 CREATE TABLE USERS(
                       USRID INT GENERATED ALWAYS AS IDENTITY,
                       USERNAME VARCHAR(50) NOT NULL,
-                      EMAIL VARCHAR(254) NOT NULL,
                       PASSWORD TEXT NOT NULL,
-                      SALT TEXT NOT NULL
+                      EMAIL VARCHAR(254) NOT NULL,
+                      SALT TEXT NOT NULL,
+                      ISADMIN BOOLEAN NOT NULL
 );
