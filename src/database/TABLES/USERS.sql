f6685665df0d985177777430371594515e5204d3
ZournaGs <zournatzisiwn15@gmail.com>
2025-05-19 13:06:33 +0300

    Fixed primary keys on USERS and TRANSACTIONS and added foreign key constraints on TRANSACTIONS

--- src/database/TABLES/USERS.sql
+++ src/database/TABLES/USERS.sql
@@ -1,5 +1,5 @@
 CREATE TABLE USERS(
-                      USRID INT GENERATED ALWAYS AS IDENTITY,
+                      USRID INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
                       USERNAME VARCHAR(50) NOT NULL,
                       PASSWORD TEXT NOT NULL,
                       EMAIL VARCHAR(254) NOT NULL,
